921f50f8054b12ddc97c584a21fb061034c3ffc3
Rasyid-deve <renaldirasyed@gmail.com>
2022-04-10 10:32:31 +0700

    edit kolom input

--- e-commerce/src/pages/Blog.jsx
+++ e-commerce/src/pages/Blog.jsx
@@ -76,7 +76,7 @@ class Blog extends Component {
             <label htmlFor="nomer unik" className="col-sm-2 col-form-label">
               Nomer Unik
             </label>
-            <div className="col-sm-10">
+            <div className="col-sm-5">
               <input type="text" className="form-control" id="uniqueNo" name="uniqueNo" placeholder="Nomer Unik" onChange={this.handleTambahBlog} />
             </div>
           </div>
@@ -84,7 +84,7 @@ class Blog extends Component {
             <label htmlFor="name" className="col-sm-2 col-form-label">
               Nama
             </label>
-            <div className="col-sm-10">
+            <div className="col-sm-5">
               <input type="text" className="form-control" id="name" name="name" placeholder="Nama" onChange={this.handleTambahBlog} />
             </div>
           </div>
@@ -92,7 +92,7 @@ class Blog extends Component {
             <label htmlFor="brand" className="col-sm-2 col-form-label">
               Brand
             </label>
-            <div className="col-sm-10">
+            <div className="col-sm-5">
               <input type="text" className="form-control" id="brand" name="brand" placeholder="Brand" onChange={this.handleTambahBlog} />
             </div>
           </div>
@@ -100,7 +100,7 @@ class Blog extends Component {
             <label htmlFor="status" className="col-sm-2 col-form-label">
               Status
             </label>
-            <div className="col-sm-10">
+            <div className="col-sm-5">
               <select className="form-control" id="status" name="status" onChange={this.handleTambahBlog}>
                 <option value="">Pilih Status</option>
                 <option value="Ready">Ready</option>
@@ -111,7 +111,7 @@ class Blog extends Component {
           </div>
           <div className="form-group row">
             <label htmlFor="status" className="col-sm-2 col-form-label"></label>
-            <div className="col-sm-10">
+            <div className="col-sm-5">
               <button className="btn btn-primary" onClick={this.handleTombolSimpan}>
                 Tambah
               </button>
